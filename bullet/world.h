3f8dd73a55ce7a3e88153993cba4d263139d9f84
Paul Michalik <paul.michalik@outlook.com>
2015-08-02 16:29:44 +0200

    Made implementation accessible to tests.

--- bullet/world.h
+++ bullet/world.h
@@ -3,16 +3,27 @@
 #include <btBulletCollisionCommon.h>
 #include <btBulletDynamicsCommon.h>
 #include <BulletCollision/Gimpact/btGImpactCollisionAlgorithm.h>
-#include <cold-md.contract/cold-md.h>
+#include <cold-md/contract/cold-md.h>
 #include <memory>
 
 namespace cold {
     namespace bullet {
         class world : public ::cold::contract::world {
+        protected:
             std::unique_ptr<btDbvtBroadphase> m_col_broadphase;
             std::unique_ptr<btDefaultCollisionConfiguration> m_col_configuration;
             std::unique_ptr<btCollisionDispatcher> m_col_dispatcher;
             std::unique_ptr<btCollisionWorld> m_col_world;
+        protected:
+            btCollisionWorld* get_world()
+            {
+                return m_col_world.get();
+            }
+
+            world* add_object(btTriangleIndexVertexArray const& p_triangles)
+            {
+                
+            }
 
             virtual void dispose() override
             {
